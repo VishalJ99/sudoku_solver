00e14fca94d9a686454cbeee8e2b1d9b0ce46765
Vishal Jain <vj279@cam.ac.uk>
2023-12-03 12:45:02 +0000

    Implement first working version of utils.validate_sudoku_input
    Introduce the first working version of the validate_sudoku_input
    function in the utils module. This function validates and corrects
    the format of Sudoku board representations from a list of strings,
    ensuring adherence to the specified format. Corrections do not modify
    add or remove numeric digits, instead raising a ValueError and exiting
    if such a change is required.
    
    Key Improvements:
     - Add a verbose flag for optional detailed output regarding corrections.
     - Enhance clarity and standardisation of warning and error messages.
     - Improve docstring clarity by detailing correction logic, adding an
       examples section, a new Raises section for Sphinx, and a TODO section
       with future improvements for the function.
     - Fix a basic bug where empty lines were not being removed.
     - Add test cases in test/test_utils.py to validate various input scenarios
       for validate_sudoku_input. These include tests with good, bad, and
       correctable Sudoku board inputs, ensuring comprehensive validation and
       error handling.
     - Add a basic unit test in test/test_utils to run the testing

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,16 @@
 import re
 
 
-def validate_sudoku_input(input_string):
+def validate_sudoku_input(input_string, verbose=False):
     """
-    Validates an input string for a sudoku board using regex pattern matching,
-    ensuring it matches format specified in README.md.
+    TODO:
+    - Merge white space and new line removal into one loop
+    - Improve print statements to format sudoku board properly when showing corrections
+    - Reject obviously incorrect boards (e.g. 2 numbers in same row)
+    - Warning messages should display row number (e.g. [WARNING] White space found in row 1)
+
+    Validates a list of strings describing rows of a sudoku board.
+    Uses regex pattern matching to check input against format in README.md.
 
     Will correct for:
         - Whitespace in the input string
@@ -12,41 +18,108 @@ def validate_sudoku_input(input_string):
         - Incorrect separator rows (raises error if digits in row)
         - Incorrect delimiters in num rows (raises error if num digits between delimiters != 3)
 
+    Logic used for seperator row correction involves first checking if the row contains any digits.
+    If it does not, seperator row is assumed to be valid and is reformatted to the correct pattern.
+    If it does, ValueError is raised.
+
+    Logic used for number row correction involves first checking if the row matches an alternate
+    valid pattern. Alternate valid patterns are rows that match against the following regex:
+    r"^\\D?\\d{3}\\D\\d{3}\\D\\d{3}\\D?$"  (note that backslashes are escaped).
+    i.e rows that contain triplets of integers separated by any non-digit character.
+    If the row matches an alternate valid pattern, the triplets are extracted and joined with |.
+    If it does not, ValueError is raised.
+
     Parameters
     ----------
     input_string : list
-        list of strings, each string representing a row of the sudoku board txt file
+        List of strings, where each string element represents a row of the sudoku board txt file.
+        A typical argument for this param would be the output of f.readlines() on the board file.
+    verbose : bool, optional
+        If True, prints warning messages when correcting input string, by default False.
 
     Returns
     -------
-    bool : True if input string is valid, False otherwise
+    bool
+        Returns True if the input string is a valid Sudoku board representation.
+        Else raises ValueError if the input contains irrecoverable formatting issues.
+
+    Raises
+    ------
+    ValueError
+        If the number of rows after basic cleaning is not equal to 11,
+        or if any row does not match the expected pattern, and cannot be corrected.
+
+
+    Examples
+    --------
+    >>> with open("test/test_boards/good_input.txt", "r") as f:
+    ...     good_input = f.readlines()
+    >>> validate_sudoku_input(good_input)
+    True
+
+    >>> with open("test/test_boards/bad_input_1.txt", "r") as f:
+    ...     bad_input_1 = f.readlines()
+    >>> validate_sudoku_input(bad_input_1)
+    Traceback (most recent call last):
+
+    ValueError: [ERROR] Number row does not match expected pattern
+    Found:
+    000|600|00
+    Expected format:
+    123|456|789
+
+    >>> with open("test/test_boards/correctable_input.txt", "r") as f:
+    ...     correctable_input = f.readlines()
+    >>> validate_sudoku_input(correctable_input)
+    True
     """
-    # Define a regex pattern for the desired numerical row
+    # define a regex pattern for the desired numerical row
     number_row_pattern = re.compile(r"^\d{3}\|\d{3}\|\d{3}$")
 
-    # Define a regex pattern an alternate general numerical row
+    # define a regex pattern an alternate general numerical row
     alt_number_row_pattern = re.compile(r"^\D?\d{3}\D\d{3}\D\d{3}\D?$")
 
-    # Define a regex pattern for the desired separator row
+    # define a regex pattern for the desired separator row
     separator_row_pattern = re.compile(r"^---\+---\+---$")
 
-    for row in input_string:
-        if bool(re.search(r"\s", row.rstrip())):
-            # Remove all white spaces from the input
-            print("[WARNING] Input string contains whitespace. Removing whitespace...")
-            input_string = "".join(row.split())
-            print(f"[INFO] Input string after removing whitespace:\n{input_string}")
+    white_space_found = False
+    empty_line_found = False
 
-            # Check if empty string
-            if not input_string:
-                print("[INFO] Removing empty row...")
-                # remove row from input string
-                input_string.remove(row)
+    # remove any white spaces
+    for idx, row in enumerate(input_string):
+        if bool(re.search(r"\s", row.rstrip())):
+            input_string[idx] = "".join(row.split())
+            white_space_found = True
+
+    if white_space_found and verbose:
+        warning_message = (
+            f"[WARNING] White space found in input string\n"
+            f"Found:\n{input_string}\n"
+            f"Replacing with:\n{input_string}\n"
+        )
+        print(warning_message)
+
+    # remove any empty lines
+    for idx, row in enumerate(input_string):
+        if not row.rstrip():
+            input_string.pop(idx)
+            empty_line_found = True
+
+    if empty_line_found and verbose:
+        warning_message = (
+            f"[WARNING] Empty lines found in input string\n"
+            f"Found:\n{input_string}\n"
+            f"Removing empty lines\n"
+            f"Result:\n{input_string}\n"
+        )
+        print(warning_message)
 
     # Check if there are 11 lines (9 rows of numbers and 2 separators)
     if len(input_string) != 11:
-        raise ValueError("[ERROR] Input string does not have 11 rows")
-        return False
+        error_message = "[ERROR] Input string does not have 11 rows\nInput string:\n"
+        for i, row in enumerate(input_string, start=1):
+            error_message += f"{i}: {row}\n"
+        raise ValueError(error_message)
 
     # Check each line against the appropriate pattern
     for i, line in enumerate(input_string):
@@ -57,50 +130,50 @@ def validate_sudoku_input(input_string):
                 if not bool(re.search(r"\d", line)):
                     # replace the incorrect separator with the correct one
                     input_string[i] = "---+---+---"
-                    print(
-                        f"""[WARNING] Separator row does not match expected pattern,
-                            Found:\n{line}\nReplacing with:\n{input_string[i]}"""
-                    )
+                    if verbose:
+                        warning_message = (
+                            f"[WARNING] Separator row does not match expected pattern\n"
+                            f"Found:\n{line}\n"
+                            f"Replacing with:\n{input_string[i]}\n"
+                        )
+                        print(warning_message)
                 else:
-                    raise ValueError(
-                        f"""[ERROR] Separator row does not match expected pattern,
-                                        Found:\n{line}\nExpected:\n---+---+---\n
-                                        Please see README.md for more information."""
+                    error_message = (
+                        f"[ERROR] Separator row does not match expected pattern\n"
+                        f"Found:\n{line}\n"
+                        f"Expected:\n---+---+---\n\n"
+                        f"Please see README.md for more information."
                     )
+                    raise ValueError(error_message)
         else:
             # Other lines should match the number row pattern
             if not number_row_pattern.match(line):
-                # check if row matches a generally acceptable pattern
+                # check if row matches an alternate valid pattern
                 if alt_number_row_pattern.match(line):
                     # extract the triple digits groupings rom the row
                     groups = re.findall(r"\d{3}", line)
 
                     # join the groups with the correct delimiter
                     input_string[i] = "|".join(groups)
-
-                    print(
-                        f"""[WARNING] Number row does not match expected pattern, Found:\n{line}'
-                            f'\nReplacing with:\n{input_string[i]}"""
-                    )
+                    if verbose:
+                        warning_message = (
+                            f"[WARNING] Number row does not match expected pattern\n"
+                            f"Found:\n{line}\n"
+                            f"Replacing with:\n{input_string[i]}\n"
+                        )
+                        print(warning_message)
                 else:
-                    raise ValueError(
-                        f"""[ERROR] Number row does not match expected pattern,
-                                        Found:\n{line}\nExpected:\n123|456|789\n
-                                        Please see README.md for more information."""
+                    error_message = (
+                        f"[ERROR] Number row does not match expected pattern\n"
+                        f"Found:\n{line}\n"
+                        f"Expected format:\n123|456|789\n\n"
+                        f"Please see README.md for more information."
                     )
+                    raise ValueError(error_message)
+
     # If all checks pass
     return True
 
 
 def parse_input(lines):
     pass
-
-
-# with open('test/test_boards/good_input.txt', 'r') as f:
-#     input_string = f.readlines()
-#     if validate_sudoku_input(input_string):
-#         print('Input string is valid')
-
-# for row in input_string:
-#     print(row.rstrip())
-#     print(bool(re.search(r'\s', row.rstrip())))
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,11 +1,29 @@
 from src import utils
+import pytest
 
 
 def test_sudoku_board_validation():
     with open("test/test_boards/good_input.txt", "r") as f:
-        input_string = f.readlines()
+        good_input = f.readlines()
+    with open("test/test_boards/bad_input_1.txt", "r") as f:
+        bad_input_1 = f.readlines()
+    with open("test/test_boards/bad_input_2.txt", "r") as f:
+        bad_input_2 = f.readlines()
+    with open("test/test_boards/bad_input_3.txt", "r") as f:
+        bad_input_3 = f.readlines()
+    with open("test/test_boards/bad_input_4.txt", "r") as f:
+        bad_input_4 = f.readlines()
+    with open("test/test_boards/correctable_input.txt", "r") as f:
+        correctable_input = f.readlines()
 
-    assert utils.validate_sudoku_input(input_string)
+    assert utils.validate_sudoku_input(good_input)
+    assert utils.validate_sudoku_input(correctable_input)
+
+    with pytest.raises(ValueError):
+        utils.validate_sudoku_input(bad_input_1)
+        utils.validate_sudoku_input(bad_input_2)
+        utils.validate_sudoku_input(bad_input_3)
+        utils.validate_sudoku_input(bad_input_4)
 
 
 def test_sudoku_parser():
