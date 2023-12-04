f594bc24b46778bcded467b1dc7b1ff7f2a3addc
Vishal Jain <vj279@cam.ac.uk>
2023-12-04 10:59:38 +0000

    Update test board paths for validation unit tests
    Make use of the new path for validation test boards

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,17 +4,17 @@ import pytest
 
 
 def test_sudoku_board_validation():
-    with open("test/test_boards/good_input.txt", "r") as f:
+    with open("test/test_validation_boards/good_input.txt", "r") as f:
         good_input = f.readlines()
-    with open("test/test_boards/bad_input_1.txt", "r") as f:
+    with open("test/test_validation_boards/bad_input_1.txt", "r") as f:
         bad_input_1 = f.readlines()
-    with open("test/test_boards/bad_input_2.txt", "r") as f:
+    with open("test/test_validation_boards/bad_input_2.txt", "r") as f:
         bad_input_2 = f.readlines()
-    with open("test/test_boards/bad_input_3.txt", "r") as f:
+    with open("test/test_validation_boards/bad_input_3.txt", "r") as f:
         bad_input_3 = f.readlines()
-    with open("test/test_boards/bad_input_4.txt", "r") as f:
+    with open("test/test_validation_boards/bad_input_4.txt", "r") as f:
         bad_input_4 = f.readlines()
-    with open("test/test_boards/correctable_input.txt", "r") as f:
+    with open("test/test_validation_boards/correctable_input.txt", "r") as f:
         correctable_input = f.readlines()
 
     assert utils.validate_sudoku_input(good_input)
@@ -41,7 +41,7 @@ def test_sudoku_parser():
             [0, 0, 0, 6, 0, 0, 0, 0, 0],
         ]
     )
-    with open("test/test_boards/good_input.txt", "r") as f:
+    with open("test/test_validation_boards/good_input.txt", "r") as f:
         good_input = f.readlines()
 
     assert np.array_equal(utils.parse_sudoku_input(good_input), expected_answer)
