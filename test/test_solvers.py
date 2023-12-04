642f589679903663b313c5ea6639b461f6721b97
Vishal Jain <vj279@cam.ac.uk>
2023-12-04 13:13:51 +0000

    Add unit test for BacktrackingSolver and refactor test file paths
    Implement test_solvers.test_backtracking_solver to validate the functionality of the BacktrackingSolver class. Ensure the solver accurately solves Sudoku
    puzzles by comparing its output with known correct solution for
    test_solver_boards/easy_1.
    
    Refactor paths for solver test boards to the global scope of test_solvers.
    This change facilitates access to these paths in other unit tests,
    enhancing modularity and robustness of the test suite.

--- test/test_solvers.py
+++ test/test_solvers.py
@@ -2,11 +2,12 @@ from src import solvers, utils
 import numpy as np
 import os
 
+EASY_BOARD_1_PATH = "test/test_solver_boards/easy_1.txt"
+EASY_BOARD_1_SOLUTION_PATH = "test/test_solver_boards/easy_1_solution.txt"
+TEST_SAVE_PATH = "test/test_solver_boards/test_save.txt"
+
 
 def test_sudoku_solver_class():
-    EASY_BOARD_1_PATH = "test/test_solver_boards/easy_1.txt"
-    TEST_SAVE_PATH = "test/test_solver_boards/test_save.txt"
-
     # load in a board
     with open(EASY_BOARD_1_PATH, "r") as f:
         good_input = f.readlines()
@@ -27,27 +28,27 @@ def test_sudoku_solver_class():
     assert np.array_equal(solver.board, board)
 
     # check that the solver can check if a move is valid
-    assert solver.check_valid(0, 2, 5)
+    assert solver.check_valid(0, 2, 3)
 
     # check that the solver can check if a move is invalid
-    assert not solver.check_valid(0, 2, 3)
+    assert not solver.check_valid(0, 2, 5)
 
     # check that the solver can find the next empty space
     assert solver.find_empty() == (0, 2)
 
     # check that the solver can print the board in a nice format
     exp_str = (
-        "9 1 . | 3 4 . | . . 7\n"
-        ". 8 3 | . 9 7 | . 5 .\n"
-        "4 2 7 | . . . | . 1 .\n"
+        "9 1 . | . . . | 4 2 7\n"
+        ". . . | . . 3 | 9 1 5\n"
+        "2 5 4 | 7 . . | 6 8 .\n"
         "------+-------+------\n"
-        ". . 2 | 6 8 . | 4 . .\n"
-        "7 . 4 | 2 . 9 | . . .\n"
-        ". . 8 | . 3 4 | 1 6 .\n"
+        "4 7 . | . 8 6 | . 3 2\n"
+        ". 6 . | 4 . . | . . 8\n"
+        "5 . . | . 1 2 | . 6 .\n"
         "------+-------+------\n"
-        "8 . . | . . . | . 4 .\n"
-        ". . 9 | . . . | 7 2 6\n"
-        ". 5 6 | . . 3 | 8 . 1\n"
+        "3 4 . | 6 2 . | . . 1\n"
+        ". . . | 3 . . | . . .\n"
+        ". 2 6 | . . 8 | . . 9\n"
     )
 
     assert str(solver) == exp_str
@@ -62,4 +63,24 @@ def test_sudoku_solver_class():
         saved_board_content = f2.read()
 
     assert original_board_content == saved_board_content
+    os.remove(TEST_SAVE_PATH)
+
+
+def test_backtracking_solver():
+    # load in a board
+    with open(EASY_BOARD_1_PATH, "r") as f:
+        good_input = f.readlines()
+
+    with open(EASY_BOARD_1_SOLUTION_PATH, "r") as f:
+        known_solution = f.read()
+
+    board = utils.parse_sudoku_input(good_input)
+    bt_solver = solvers.BacktrackingSolver(board)
+    bt_solver.solve()
+    bt_solver.save_board(TEST_SAVE_PATH)
+
+    with open(TEST_SAVE_PATH, "r") as f:
+        bt_solution = f.read()
+
+    assert bt_solution == known_solution
     os.remove(TEST_SAVE_PATH)
