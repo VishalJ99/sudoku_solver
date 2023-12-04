c930248b835e23448dc3f992bf405be26fec82cd
Vishal Jain <vj279@cam.ac.uk>
2023-12-04 11:52:15 +0000

    Implement BacktrackingSolver for Sudoku puzzles
    - Add BacktrackingSolver as a subclass of SudokuSolver.
    - Implement the backtracking algorithm in the solve method to recursively
      solve Sudoku puzzles.
    
    This addition introduces a practical and widely-used approach to solving
    Sudoku puzzles.

--- src/solvers.py
+++ src/solvers.py
@@ -96,3 +96,52 @@ class SudokuSolver:
             else:
                 board_str += "\n"
         return board_str
+
+
+class BacktrackingSolver(SudokuSolver):
+    """
+    Class for solving sudoku puzzles using a backtracking algorithm
+    """
+
+    def __init__(self, board):
+        """
+        Initialize the BacktrackingSolver object
+
+        Parameters
+        ----------
+        board : numpy.ndarray
+            2D numpy array representing the sudoku board to be solved
+        """
+        super().__init__(board)
+
+    def solve(self):
+        """
+        Solve the sudoku puzzle using a backtracking algorithm
+
+        Returns
+        -------
+        bool
+            True if the puzzle was solved, False otherwise
+        """
+        # find the next empty square
+        i, j = self.find_empty()
+
+        # if there are no empty squares, the puzzle is solved
+        if i is None:
+            return True
+
+        # check which numbers are valid in the empty square
+        for num in range(1, 10):
+            if self.check_valid(i, j, num):
+                # make the move and repeat recursively
+                self.board[i, j] = num
+                if self.solve():
+                    # puzzle was solved
+                    return True
+
+                # if the puzzle was not solved, undo the move
+                self.board[i, j] = 0
+
+        # if no numbers worked, an incorrect move was made somewhere
+        # return False to backtrack
+        return False
