4e9111b4b3cbe79b1aba7a959d309f9329c935c4
Vishal Jain <vj279@cam.ac.uk>
2023-12-14 15:31:12 +0000

    removed temp helper script

--- split_files.py
+++
@@ -1,9 +0,0 @@
-input_file = input("Enter the path of the input text file: ")
-
-with open(input_file, "r") as file:
-    lines = file.readlines()
-
-for i, line in enumerate(lines):
-    output_file = f"challenging_sudoku_{i}.txt"
-    with open(output_file, "w") as file:
-        file.write(line)
