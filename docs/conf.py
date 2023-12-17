e2afcc82dd6714270a937d68ecbea57164b744ae
Vishal Jain <vj279@cam.ac.uk>
2023-12-17 01:41:47 +0000

    Add Sphinx Files for AutoDoc functionality
    Adding sphinx doc files for automatic documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -12,15 +12,19 @@
 #
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath("../src"))
+sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = "SudokuSolver"
-copyright = "2023, Vishal Jain"
-author = "Vishal Jain"
+project = 'Sudoku Solver'
+copyright = '2023, Vishal Jain'
+author = 'Vishal Jain'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -29,17 +33,23 @@ author = "Vishal Jain"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon"
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
+autodoc_default_options = {
+    'special-members': '__init__',
+    # other options...
+}
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +57,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,14 +1,14 @@
-Welcome to SudokuSolver's documentation!
-========================================
+.. Sudoku Solver documentation master file, created by
+   sphinx-quickstart on Sat Dec 16 23:37:24 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Sudoku Solver's documentation!
+=========================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+   
+   modules
 
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
